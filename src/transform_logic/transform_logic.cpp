becbbce5bf0c8f5eeea57cba62d0288676a3c213
Botir Akhmedov <botir.akhmedov@gmail.com>
2024-11-20 19:20:03 +0500

    Add test for transform lib

--- src/transform_logic/transform_logic.cpp
+++ src/transform_logic/transform_logic.cpp
@@ -6,3 +6,8 @@ void transform_logic::test()
     std::shared_ptr<repository> repo = std::make_shared<repository>();
     repo->test();
 }
+
+std::string transform_logic::parse_to_syllable(std::string inp_word)
+{
+    return std::string();
+}
--- src/transform_logic/transform_logic.h
+++ src/transform_logic/transform_logic.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <string>
 
 class transform_logic
 {
     public:
     transform_logic(){}
     void test();
+    std::string parse_to_syllable(std::string inp_word);
 };
