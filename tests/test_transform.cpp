062963691481ade5960e8c64444e74d1f46930eb
Botir Akhmedov <botir.akhmedov@gmail.com>
2024-11-28 14:48:51 +0500

    gtest try

--- tests/test_transform.cpp
+++ tests/test_transform.cpp
@@ -1,32 +1,29 @@
-#include <catch2/catch_test_macros.hpp>
+#include <gtest/gtest.h>
 
 #include "transform_logic.h"
 #include "word_extractor.h"
 
-TEST_CASE("syllable")
+TEST(WordExtractortest, replaceAll)
 {
     transform_logic tl;
     //tl.word_to_letters("O‘ma’rifiy");
     std::string source {"O‘ma’rifiy"};
     tl.replace_all(source, "‘","`");
     tl.replace_all(source, "’","'");
-    REQUIRE(source == "O`ma'rifiy");
+    EXPECT_TRUE(source == "O`ma'rifiy");
 }
 
-TEST_CASE("word_extractor")
+TEST(WordExtractortest, asciiConvertion)
 {
-    SECTION( "Get clean word" ) {
-        REQUIRE(word_extractor::get_clean_word(" ()[]/O‘ma’rifiy (sosd)") == "O‘ma’rifiy");
-        REQUIRE(word_extractor::get_clean_word(" O‘ma’ri-fiy (sosd)") == "O‘ma’ri-fiy");
-        REQUIRE(word_extractor::get_clean_word(" -O‘ma’rifiy,: (sosd)") == "O‘ma’rifiy");
-    }
-
-    SECTION( "Get ASCII word" ) {
-        REQUIRE(word_extractor::convert_to_one_letter_ascii("O‘ma’rifiy") == "0ma'rifiy");
-        REQUIRE(word_extractor::convert_to_one_letter_ascii("Chang") == "cang");
-        REQUIRE(word_extractor::convert_to_one_letter_ascii("tasdig‘i") == "tasdi9i");
-        REQUIRE(word_extractor::convert_to_one_letter_ascii("tasdig`i") == "tasdi9i");
-        REQUIRE(word_extractor::convert_to_one_letter_ascii("toshqin") == "towqin");
-    }
+    EXPECT_TRUE(word_extractor::get_clean_word(" ()[]/O‘ma’rifiy (sosd)") == "O‘ma’rifiy");
+    EXPECT_TRUE(word_extractor::get_clean_word(" O‘ma’ri-fiy (sosd)") == "O‘ma’ri-fiy");
+    EXPECT_TRUE(word_extractor::get_clean_word(" -O‘ma’rifiy,: (sosd)") == "O‘ma’rifiy");
+    
+    EXPECT_TRUE(word_extractor::convert_to_one_letter_ascii("O‘ma’rifiy") == "0ma'rifiy");
+    EXPECT_TRUE(word_extractor::convert_to_one_letter_ascii("Chang") == "cang");
+    EXPECT_TRUE(word_extractor::convert_to_one_letter_ascii("tasdig‘i") == "tasdi9i");
+    EXPECT_TRUE(word_extractor::convert_to_one_letter_ascii("tasdig`i") == "tasdi9i");
+    EXPECT_TRUE(word_extractor::convert_to_one_letter_ascii("toshqin") == "towqin");
+    
     
 }
